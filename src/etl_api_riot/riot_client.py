0a3cdb8063c3469090ae3d78cebd1379ae68f3c2
joaohsilva <joaohasilva416@gmail.com>
2026-01-29 15:00:18 -0300

    feat: Implementa busca de partidas e refatora request helper

--- src/etl_api_riot/riot_client.py
+++ src/etl_api_riot/riot_client.py
@@ -15,9 +15,9 @@ class RiotClient():
         }
     
     # Função para pegar o site
-    def _make_request(self, endpoint):
-        url_completa = f'{self.url_base}{endpoint}' # Concatena url base com endpoint
-        response = requests.get(url_completa, headers=self.headers) # Requisição do site
+    def _make_request(self, endpoint, params=None):
+        url_full = f'{self.url_base}{endpoint}' # Concatena url base com endpoint
+        response = requests.get(url_full, params=params, headers=self.headers) # Requisição do site
         data = response.json() # Armazena a resposta em um json
 
         # Verifica se o status code é diferente de 200
@@ -30,9 +30,17 @@ class RiotClient():
     # Função para pegar o puuid
     def get_puuid(self, game_name, tag_line):
         endpoint = f'/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}' # Define o endpoint
-        valor = self._make_request(endpoint)['puuid'] # Pega apenas o puuid
+        value = self._make_request(endpoint)['puuid'] # Pega apenas o puuid
         
-        return valor
+        return value
+    
+    # Função para pegar os id's das últimas 20 partidas
+    def get_matches(self, puuid, count=20):
+        # Lista vazia onde vai ser armazenado os ids das partidas
+        endpoint = f'/lol/match/v5/matches/by-puuid/{puuid}/ids' # Define o endpoint
+        value = self._make_request(endpoint, params={"count": count}) # Pega as 20 últimas id's
+
+        return value
     
 if __name__ == '__main__':
     # Instancia o cliente
@@ -40,8 +48,11 @@ if __name__ == '__main__':
 
     # Imprime apenas o puuid
     try:
-        meu_puuid = client.get_puuid("kojiii", "00000")
-        print(f"Puuid: {meu_puuid}")
+        my_puuid = client.get_puuid("kojiii", "00000")
+        print(f"Puuid: {my_puuid}")
+
+        my_matches = client.get_matches(my_puuid)
+        print(f"Lista das partidas: {my_matches}")
     # Imprime erro caso aconteça
     except Exception as e:
         print(f"Erro {e}")
