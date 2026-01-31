8bd10c14a3ef726b1b52da877d00abbf555e343b
joaohsilva <joaohasilva416@gmail.com>
2026-01-31 16:15:54 -0300

    feat(snowflake): implementar classe SnowflakeLoader e métodos de carga

--- src/etl_api_riot/riot_client.py
+++ src/etl_api_riot/riot_client.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 import time
 import json
+import snowflake.connector
 
 # Carrega as configurações do arquivo .env
 load_dotenv()
@@ -31,25 +32,105 @@ class RiotClient():
     
     # Função para pegar o puuid
     def get_puuid(self, game_name, tag_line):
-        endpoint = f'/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}' # Define o endpoint
+        endpoint = f"/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}" # Define o endpoint
         value = self._make_request(endpoint)['puuid'] # Pega apenas o puuid
         
         return value
     
     # Função para pegar os id's das partidas
     def get_matches(self, puuid, count=20):
-        endpoint = f'/lol/match/v5/matches/by-puuid/{puuid}/ids' # Define o endpoint
+        endpoint = f"/lol/match/v5/matches/by-puuid/{puuid}/ids" # Define o endpoint
         value = self._make_request(endpoint, params={"count": count}) # Pega as 20 últimas id's
 
         return value
     
     # Função para pegar as informações das partidas
     def get_match_details(self, match_id):
-        endpoint = f'/lol/match/v5/matches/{match_id}'
+        endpoint = f"/lol/match/v5/matches/{match_id}"
         value = self._make_request(endpoint)
 
         return value
     
+# classe para conectar e salvar dados no SnowFlake
+class SnowFlakeLoader():
+    def __init__(self):
+        # Lendo as informações de login das variáveis de ambiente
+        self.account = os.getenv("ACCOUNT")
+        self.user = os.getenv("USER")
+        self.password = os.getenv("PASSWORD")
+        self.role = os.getenv("ROLE")
+        self.warehouse = os.getenv("WAREHOUSE")
+        self.database = os.getenv("DATABASE")
+        self.schema = os.getenv("SCHEMA")
+    
+    # Função para conectar no snowflake
+    def connect_to_snowflake(self):
+        return snowflake.connector.connect(
+            account = self.account,
+            user = self.user,
+            password = self.password,
+            role = self.role,
+            warehouse = self.warehouse,
+        )
+    
+    # Função para criar a tabela
+    def create_table(self):
+        conn = self.connect_to_snowflake()
+        cursor = conn.cursor()
+
+        # Criando e usando database
+        cursor.execute("CREATE DATABASE IF NOT EXISTS RIOT_DB")
+        cursor.execute("USE DATABASE RIOT_DB")
+
+        # Criando e usando schema
+        cursor.execute("CREATE SCHEMA IF NOT EXISTS PUBLIC")
+        cursor.execute("USE SCHEMA RIOT_DB.PUBLIC")
+
+        # Criando a tabela
+        query = cursor.execute("""
+                        CREATE TABLE IF NOT EXISTS MATCHES_RAW(
+                               MATCH_ID VARCHAR(50) PRIMARY KEY,
+                               MATCH_DATA VARIANT,
+                               CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP())
+        """)
+
+        # Encerrando conexão
+        cursor.close()
+        conn.close()
+    
+    def load_data(self, match_results):
+        conn = self.connect_to_snowflake()
+        cursor = conn.cursor()
+        try:
+            # Usando database e schema
+            cursor.execute("USE DATABASE RIOT_DB")
+            cursor.execute("USE SCHEMA RIOT_DB.PUBLIC")
+
+            # for loop para percorrer cada partida
+            for match in match_results:
+                try:
+                    # Pegamos o ID de dentro do JSON
+                    match_id = match['metadata']['matchId']
+
+                    # Transformamos o dict em string JSON para enviar
+                    match_json_str = json.dumps(match)
+
+                    # Query de Insert usando PARSE_JSON
+                    # Usamos %s para evitar SQL Injection (bind variable)
+                    sql = "INSERT INTO MATCHES_RAW (MATCH_ID, MATCH_DATA) SELECT %s, PARSE_JSON(%s)"
+
+                    cursor.execute(sql, (match_id, match_json_str))
+                except KeyError:
+                    print(f"Erro: Partida sem ID encontrado. Pulando...")
+                except Exception as e:
+                    print(f"Erro ao inserir partida {match_id}: {e}")
+                    
+            conn.commit() # Confirma a transação
+            print("Commit realizado com sucesso!")
+        finally:
+            cursor.close()
+            conn.close()
+        
 def main():
     client = RiotClient()
 
@@ -60,7 +141,7 @@ def main():
         # Pegar Lista de Partidas
         matches_ids = client.get_matches(my_puuid)
 
-        # 3. Loop de Download
+        # Loop de Download
         results = []
 
         print("Iniciando download das partidas...")
@@ -72,15 +153,21 @@ def main():
             
         print(f"Sucesso! Detalhes baixados.")
 
-        # 4. Salvar Arquivo
+        # Salvar Arquivo
         print("Salvando arquivo Json...")
-        with open('matches_raw.json', 'w', encoding='utf-8') as f:
+        with open("matches_raw.json", "w", encoding="utf-8") as f:
             json.dump(results, f, indent=4, ensure_ascii=False)
             
         print("Arquivo salvo com sucesso.")
 
+        print("Iniciando carga no Snowflake...")
+        loader = SnowFlakeLoader()
+        loader.create_table() # Garante que a tabela existe
+        loader.load_data(results) # Carrega os dados para o snowflake
+        print("Carga concluída!")
+
     except Exception as e:
         print(f"Erro no processo: {e}")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
