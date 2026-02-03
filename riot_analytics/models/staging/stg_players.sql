560b2e6285506a969ea816218a3ef9faf934baef
joaohsilva <joaohasilva416@gmail.com>
2026-02-03 17:47:30 -0300

    feat(dbt): implementa staging model stg_players

---
+++ riot_analytics/models/staging/stg_players.sql
@@ -0,0 +1,34 @@
+-- Importa os dados
+with source as (
+    select
+        *
+    from
+        {{source('RIOT_DB', 'MATCHES_RAW')}}
+),
+
+-- inserir transformações
+renamed as (
+    select
+        source.MATCH_ID as id_partida,
+        player.value:puuid::VARCHAR as id_jogador,
+        player.value:riotIdGameName::VARCHAR as nome_jogador,
+        player.value:championName::VARCHAR as nome_campeao,
+
+        -- métricas de combate
+        player.value:kills::INTEGER as abates,
+        player.value:deaths::INTEGER as mortes,
+        player.value:assists::INTEGER as assistencias,
+
+        -- métricas de economia e dano
+        player.value:totalDamageDealtToChampions::INTEGER as dano_total_campeoes,
+        player.value:goldEarned::INTEGER as ouro_total,
+
+        -- resultado da partida
+        player.value:win::BOOLEAN as vitoria
+    from
+        source,
+        LATERAL FLATTEN(input => source.MATCH_DATA:info:participants) as player
+)
+
+-- select final
+select * from renamed
